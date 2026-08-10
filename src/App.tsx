ba73e457601504aafc11e2c5f376bc5abbfad173
Priyanshu84iya <priyanshu69chaurasiya@gmail.com>
2026-08-10 12:25:13 +0530

    Fix Analytics import and merge conflicts for PWA release

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,11 @@ import { AboutPage } from "@/pages/AboutPage";
 import { CopyrightPage } from "@/pages/CopyrightPage";
 import { StoreProvider, useStore } from "@/store/AppStore";
 import { RouterProvider, useRouter } from "@/store/Router";
-<<<<<<< HEAD
 import { PWAProvider } from "@/hooks/usePWA";
 import { PWAInstallBanner } from "@/components/PWAInstallBanner";
 import { PWAUpdateBanner } from "@/components/PWAUpdateBanner";
 import { OfflineBanner } from "@/components/OfflineBanner";
 import { Analytics } from "@vercel/analytics/react";
-=======
-import { Analytics } from "@vercel/analytics/react"
->>>>>>> 1a6cb457c147235dae90d9ef32efd93d318b8bf8
 
 function PageRouter() {
   const { route } = useRouter();
@@ -92,7 +88,6 @@ function Layout() {
 
 function App() {
   return (
-<<<<<<< HEAD
     <PWAProvider>
       <RouterProvider>
         <StoreProvider>
@@ -101,14 +96,6 @@ function App() {
         </StoreProvider>
       </RouterProvider>
     </PWAProvider>
-=======
-    <RouterProvider>
-      <StoreProvider>
-        <Layout />
-        <Analytics />
-      </StoreProvider>
-    </RouterProvider>
->>>>>>> 1a6cb457c147235dae90d9ef32efd93d318b8bf8
   );
 }
 
